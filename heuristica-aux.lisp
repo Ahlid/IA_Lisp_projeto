b39d291a7075735f3c2de7c4dd070e1f77162c71
Ahlid <tiagoasmont@gmail.com>
2016-12-12 11:14:40 +0000

    calculo das partilhas (Todo: conversão do tabuleiro para a metodologia na imagem partilh2)

--- heuristica-aux.lisp
+++ heuristica-aux.lisp
@@ -1,33 +1,48 @@
-(defun h-numero-partilhas-horizonta-duas-linhas-quadrados(linha1 linha2)
+(defun h-numero-partilhas-horizonta-duas-linhas-quadrados(linha1 linha2 n1 n2)
 
+  (apply '+
 
-  (mapcar
-   (lambda(x y)
-     (cond
-      ((and (= (second x) (first y)) (= 1 (second x))) 1)
-      (T 0)
-      )
-     )
+         (mapcar
+          (lambda(x y)
+            (cond
+             ((and (= 1 (verificar-n-arcos-faltam x n1)) (= 1 (verificar-n-arcos-faltam y n2)))
+              (cond
+               ((and (= (second x) (first y)) (= 1 (second x))) 1)
+               (T 0)
+               )
+              )
+             (T 0)
+             )
 
-   '((0 0 0 0) (0 0 0 0)) '((0 1 0 0)(0 0 0 0)) )
+            )
+          linha1 linha2
+          )
+
+         )
 
   )
 
-(defun h-linhas-feitas-quadrado (quadrado)
 
-(apply '+ quadrado)
-)
+(defun h-numero-partilhas-vertical (linha n1 n2)
 
+  (cond
+   ((null linha) 0)
+   ((null (second linha)) 0)
+   ((and (= 1 (third (first linha))) (= (third (first linha)) (fourth (second linha)) ) )
 
-
-(defun h-aux-arcos-horizontal(quadrados )
-
+    (cond
+     ((and (= 1 (verificar-n-arcos-faltam (first linha) n1)) (= 1 (verificar-n-arcos-faltam (second linha) n2)))
+      (+ 1 (h-numero-partilhas-vertical (cdr linha) n1 n2)) )
+     (T
+      (h-numero-partilhas-vertical (cdr linha) n1 n2)))
+    )
+   (T (h-numero-partilhas-vertical (cdr linha) n1 n2)))
 
 )
 
 
 (defun n-caixas-a-faltar-x-arcos(caixas n)
-  (apply 
+  (apply
    '+
    (mapcar
     (lambda (x)
@@ -48,4 +63,42 @@
    ((= n (- 4 (apply '+ caixa))) 1)
    (t 0)
    )
+  )
+
+
+(defun calcurar-n-partilhas-m-p (caixas n1 n2)
+
+(+ (aux-partilhas-horizontal caixas n1 n2) (aux-partilhas-vertical caixas n1 n2))
+
+)
+
+
+
+(defun get-helper()
+
+'( ((0 0 1 0) (0 0 1 1)) ((0 0 1 0)(0 0 1 1)) )
+)
+(defun get-helper2()
+
+'( ((1 1 0 0) (1 1 0 0) (1 1 0 0)) ((1 1 0 0)(1 1 0 0) (1 1 0 0)) ((1 1 0 0)(1 1 0 0) (0 0 0 0))  )
+)
+
+
+
+(defun aux-partilhas-vertical(caixas n1 n2)
+
+  (cond
+   ((null caixas) 0)
+   (T (+ (h-numero-partilhas-vertical (first caixas) n1 n2) (aux-partilha-vertical (rest caixas) n1 n2) ))
+   )
+  )
+
+(defun aux-partilhas-horizontal(caixas n1 n2)
+  (cond
+   ((null caixas) 0)
+   ((null (second caixas)) 0)
+   (T (+ (h-numero-partilhas-horizonta-duas-linhas-quadrados (first caixas) (second caixas) n1 n2 ) (aux-partilhas-horizontal (rest caixas) n1 n2)))
+
+   )
+
   )
